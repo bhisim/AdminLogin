f9b4a36ba46d02a8ace70752252d6b25b911ea6e
Asus <Asus@DESKTOP-4DUKIIU>
2021-11-10 22:42:03 +0300

    Add project files.

---
+++ AdminLogin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AdminLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/* Bahar Merdamert max 3 kez denenince login olabilen uygulama */
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Lütfen kullanıcı adınızı giriniz...");
+                string kullaniciAdi = Console.ReadLine();
+
+                Console.WriteLine("Lütfen şifrenizi giriniz...");
+                string sifre = Console.ReadLine();
+
+                if (kullaniciAdi == "admin" && sifre == "123")
+                {
+                    Console.WriteLine("Hoşgeldin admin...");
+                    break;
+                }
+                else
+                    Console.WriteLine("Şifrenizi veya kullanıcı adınızı yanlış girdiniz...");
+                    Console.WriteLine("***************************************************");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
